2f00f3fcfc9ef086c648d2dfb8b4b505c2ba91d4
yadon <doinkya0@gmail.com>
2025-04-19 08:52:47 +0000

    feat: タグ一覧取得とタグ登録のHTTPハンドラーをルーティング（mux.go）に設定

--- mux.go
+++ mux.go
@@ -1,14 +1,28 @@
 package main
 
-import "net/http"
+import (
+		"net/http"
+
+		"github.com/go-webapi-team/task-app/handler"
+		"github.com/go-webapi-team/task-app/store"
+
+		"github.com/go-chi/chi/v5"
+		"github.com/go-playground/validator/v10"
+)
 
 // NewMuxは、どのようなハンドラーの実装をどんなURLパスで公開するかルーティングする
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		// 静的解析(実装予定)のエラーを回避するため明示的に戻り値を捨てる
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+	mux.Handle("/tags", &handler.CreateTag{Store: store.Tags, Validator: v})
+	ct := &handler.CreateTag{Store: store.Tags, Validator: v}
+	mux.Post("/tags", ct.ServeHTTP)
+	lt := &handler.ListTag{Store: store.Tags}
+	mux.Get("/tags", lt.ServeHTTP)
 	return mux
 }
