7555f1ac426e811ee2e4a37fa4069c4c4a58b553
yadon <doinkya0@gmail.com>
2025-04-28 03:34:26 +0000

    fix: 無駄な空白の削除と空文字列のエラーハンドリングを追加

--- store/tag.go
+++ store/tag.go
@@ -92,7 +92,10 @@ func (r *Repository) DeleteTagFromTask(ctx context.Context, db Execer, userID in
     if err != nil {
         return err
     }
-    n, _ := res.RowsAffected()
+    n, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
     if n == 0 {
         return ErrNotFound
     }
