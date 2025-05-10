ec8a83dedd5374589b426a761478c7ce1feff358
yadon <doinkya0@gmail.com>
2025-05-10 02:15:24 +0000

    fix:不要なコメントを削除

--- entity/task.go
+++ entity/task.go
@@ -4,18 +4,6 @@ import "time"
 
 type TaskID int64
 
-// type Task struct {
-// 	ID          TaskID    `json:"id"`
-// 	UserID      int64     `json:"user_id"`
-// 	Title       string    `json:"title"`
-// 	Description string     `json:"description,omitempty"`
-// 	Deadline    *time.Time `json:"deadline,omitempty"`
-// 	Priority    int       `json:"priority"` // 1:Low 2:Normal 3:High
-// 	IsDone      bool      `json:"is_done"`
-// 	CreatedAt   time.Time `json:"created_at"`
-// 	UpdatedAt   time.Time `json:"updated_at"`
-// }
-
 type Task struct {
 	// ID タスク識別子
 	ID          TaskID      `json:"id"          example:"1"`
